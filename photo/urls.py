26c5ddd4e9c1697dbdabb220d3a58857a9ca9d1d
ImJonghan <ijhan21c@naver.com>
2024-04-11 10:33:15 +0900

    'photo'

--- photo/urls.py
+++ photo/urls.py
@@ -6,7 +6,9 @@ from . import views
 
 app_name = "photo"
 
-urlpatterns = [
-    # path("", views.BookmarkLV.as_view(), name="index"),
-    # path("<int:pk>/", views.BookmarkDV.as_view(), name="detail"),    
+urlpatterns = [    
+    path("", views.AlbumLV.as_view(), name="index"),
+    path("album", views.AlbumLV.as_view(), name="album_list"),
+    path("album/<int:pk>", views.AlbumDV.as_view(), name="album_detail"),
+    path("photo/<int:pk>", views.PhotoDV.as_view(), name="photo_detail"),
 ]
--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,28 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from .models import Album, Photo
 # Create your views here.
+
+class AlbumLV(ListView):
+    model=Album
+    # context_object_name = 'object_list'
+    # template_name='photo/album_list.html'    
+    # ListView의 기본 템플릿 : 앱이름/모델_list.html
+    # context = {'object_list':....}
+    # def get_queryset(self) -> QuerySet[Any]:        
+    #     return self.model.objects.all()    
+    # def get_object # DV
+    # def get_context_data
+
+class AlbumDV(DetailView):
+    model=Album
+    template_name='photo/album_detail.html'
+    # pk를 통해서 특정 앨범만 가져온다
+
+class PhotoDV(DetailView):
+    model=Photo
+    template_name='photo/photo_detail.html'
+class Question:pass
+class Choice:pass
